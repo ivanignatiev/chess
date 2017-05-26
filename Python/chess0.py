b29a1a9db667c36fd264f14046a328d582321e51
Ivan Ignatiev <ignatiev.ivan@gmail.com>
2017-05-26 11:23:43 +0200

    Basic output

---
+++ Python/chess0.py
@@ -0,0 +1,10 @@
+print(" abcdefgh ")
+print("8RNBQKBNR8");
+print("7PPPPPPPP7");
+print("6        6");
+print("5        5");
+print("4        4");
+print("3        3");
+print("2PPPPPPPP2");
+print("1RNBQKBNR1");
+print(" abcdefgh ")
